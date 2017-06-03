edf8c52312d928aa5591d85f319af502b31b4681
Qianxi Zhang <qianxi.zhang@hulu.com>
2017-06-03 11:08:13 +0800

    add answer to mapreduce invertindex

--- src/main/java/org/training/hadoop/mapreduce/InvertIndex.java
+++ src/main/java/org/training/hadoop/mapreduce/InvertIndex.java
@@ -8,10 +8,16 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class InvertIndex {
 
@@ -19,19 +25,34 @@ public class InvertIndex {
     public void map(Object key,
                     Text value,
                     Context context) throws IOException, InterruptedException {
-      // TODO please implement this method
+      //Get the name of the file using context.getInputSplit()method
+      String fileName = ((FileSplit) context.getInputSplit()).getPath().getName();
+      //Split the line in words
+      StringTokenizer itr = new StringTokenizer(value.toString());
+      while (itr.hasMoreTokens()) {
+        //For each word emit word as key and file name as value
+        context.write(new Text(itr.nextToken()), new Text(fileName));
+      }
     }
-  }
-
-  public static class IndexCombiner extends Mapper<Text, Text, Text, Text> {
-
   }
 
   public static class IndexReducer extends Reducer<Text, Text, Text, Text> {
     public void reduce(Text key,
                        Iterable<Text> values,
                        Context context) throws IOException, InterruptedException {
-      // TODO please implement this method
+      // Declare the Hash Map to store File name as key to compute
+      // and store number of times the filename is occurred for as value
+      Map<String, Integer> map = new HashMap<String, Integer>();
+      for (Text fileText : values) {
+        String file = fileText.toString();
+        if (map.containsKey(file)) {
+          map.put(file, map.get(file) + 1);
+        }
+        else {
+          map.put(file, 1);
+        }
+      }
+      context.write(key, new Text(map.toString()));
     }
   }
 
@@ -44,12 +65,28 @@ public class InvertIndex {
       System.exit(2);
     }
     Job job = Job.getInstance(conf, "Invert Index");
-    // TODO please set correct mapper and reducer for this class as well as output key/value class
+
+    job.setJarByClass(InvertIndex.class);
+    job.setMapperClass(IndexMapper.class);
+    job.setReducerClass(IndexReducer.class);
+
+    //Defining the output key and value class for the mapper
+    job.setMapOutputKeyClass(Text.class);
+    job.setMapOutputValueClass(Text.class);
+
+    //Defining the output key and value class for the reducer
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(Text.class);
+
+    job.setInputFormatClass(TextInputFormat.class);
+    job.setOutputFormatClass(TextOutputFormat.class);
+
     for (int i = 0; i < otherArgs.length - 1; ++i) {
       FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
     }
     FileOutputFormat.setOutputPath(job,
         new Path(otherArgs[otherArgs.length - 1]));
+
     System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
 }
